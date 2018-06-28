8b54e036f366a92977630fcb53e3d065ad605c25
pavloskii <pavlo.igor@gmail.com>
2018-06-28 22:49:05 +0200

    Initial commit

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Toolbar.css';
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton/DrawerToggleButton';
+
+const toolbar = props => (
+    <header className="toolbar">
+        <nav className="toolbar__navigation">
+            <div>
+                <DrawerToggleButton />
+            </div>
+            <div className="toolbar__logo"><a href="/">THE LOGO</a></div>
+            <div className="spacer" />
+            <div className="toolbar_navigation-items">
+                <ul>
+                    <li><a href="/">Products</a></li>
+                    <li><a href="/">Users</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header> //18:51 Build a responsive Navigation Bar and side drawer
+)
+
+export default toolbar;
